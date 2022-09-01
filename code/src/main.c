199aaef832555850c662c078fc8a5c69dae91038
sudjoao <isudjoao@gmail.com>
2022-09-01 15:45:09 -0300

    reestructure code

--- code/src/main.c
+++ code/src/main.c
@@ -2,9 +2,14 @@
 #include <unistd.h>        
 #include <fcntl.h>         
 #include <termios.h>       
-#include <string.h>      
+#include <string.h>
+#include "crc16.h"
 
-char getOptionValue(int option){
+char getCode(int option){
+    return option < 4 ? 0x23 : 0x16;
+}
+
+char getSubCode(int option){
     if(option == 1){
         return 0xC1;
     }else if(option == 2){
@@ -38,17 +43,48 @@ void debugMenu(){
     printf("8. Envia valor do Temporizador\n");
 }
 
+void sendMessage(char code, char subcode, int uart0_filestream ){
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+    int i =0;
+    printf("hmm 2\n");
+    p_tx_buffer = &tx_buffer[0];
+    tx_buffer[i++] = 0x01;
+    tx_buffer[i++] = code;
+    tx_buffer[i++] = subcode;
+    tx_buffer[i++] = 3;
+    tx_buffer[i++] = 9;
+    tx_buffer[i++] = 1;
+    tx_buffer[i++] = 0;
+    unsigned short crc = calcula_CRC(tx_buffer, sizeof(tx_buffer));
+    tx_buffer[i++] = crc;
+    printf("Buffers de memória criados!\n");
+    
+    if (uart0_filestream != -1)
+    {
+        printf("Escrevendo caracteres na UART ...");
+        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
+        if (count < 0)
+        {
+            printf("UART TX error\n");
+        }
+        else
+        {
+            printf("escrito.\n");
+        }
+    }
+
+}
+
 void printOutput(int option, unsigned char* buffer, int size){}
 
 int main(int argc, const char * argv[]) {
 
     int uart0_filestream = -1;
     int option;
-    int mat[4] = {3, 9, 1, 0};
     debugMenu();
     scanf("%d", &option);
 
-    char optionValue = getOptionValue(option);
 
     uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
     if (uart0_filestream == -1)
@@ -68,50 +104,32 @@ int main(int argc, const char * argv[]) {
     tcflush(uart0_filestream, TCIFLUSH);
     tcsetattr(uart0_filestream, TCSANOW, &options);
 
-    unsigned char tx_buffer[20];
-    unsigned char *p_tx_buffer;
-    
-    p_tx_buffer = &tx_buffer[0];
-    *p_tx_buffer++ = optionValue;
-    memcpy(*p_tx_buffer, mat, 4);
-
-    printf("Buffers de memória criados!\n");
-    
-    if (uart0_filestream != -1)
-    {
-        printf("Escrevendo caracteres na UART ...");
-        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
-        if (count < 0)
-        {
-            printf("UART TX error\n");
-        }
-        else
-        {
-            printf("escrito.\n");
-        }
-    }
+    char code = getCode(option); 
+    char subcode = getSubCode(option);
+    printf("hmm\n");
+    sendMessage(code, subcode, uart0_filestream);
 
     sleep(1);
 
-    if (uart0_filestream != -1)
-    {
+    // if (uart0_filestream != -1)
+    // {
     
-        unsigned char rx_buffer[256];
-        int data;
-        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);     
-        if (rx_length < 0)
-        {
-            printf("Erro na leitura.\n");
-        }
-        else if (rx_length == 0)
-        {
-            printf("Nenhum dado disponível.\n");
-        }
-        else
-        {
-            printOutput(option, rx_buffer, rx_length);
-        }
-    }
+    //     unsigned char rx_buffer[256];
+    //     int data;
+    //     int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);     
+    //     if (rx_length < 0)
+    //     {
+    //         printf("Erro na leitura.\n");
+    //     }
+    //     else if (rx_length == 0)
+    //     {
+    //         printf("Nenhum dado disponível.\n");
+    //     }
+    //     else
+    //     {
+    //         printOutput(option, rx_buffer, rx_length);
+    //     }
+    // }
 
     close(uart0_filestream);
    return 0;
