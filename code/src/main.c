376cbae63bab7998da9414d042b40f3b2351c594
sudjoao <isudjoao@gmail.com>
2022-09-01 16:01:33 -0300

    fix send message logic

--- code/src/main.c
+++ code/src/main.c
@@ -43,27 +43,20 @@ void debugMenu(){
     printf("8. Envia valor do Temporizador\n");
 }
 
+void printOutput(int option, unsigned char* buffer, int size){}
+
 void sendMessage(char code, char subcode, int uart0_filestream ){
+    unsigned char infos[7] = {0x01, code, subcode, 3, 9, 1, 0};
     unsigned char tx_buffer[20];
-    unsigned char *p_tx_buffer;
-    int i =0;
-    printf("hmm 2\n");
-    p_tx_buffer = &tx_buffer[0];
-    tx_buffer[i++] = 0x01;
-    tx_buffer[i++] = code;
-    tx_buffer[i++] = subcode;
-    tx_buffer[i++] = 3;
-    tx_buffer[i++] = 9;
-    tx_buffer[i++] = 1;
-    tx_buffer[i++] = 0;
-    unsigned short crc = calcula_CRC(tx_buffer, sizeof(tx_buffer));
-    tx_buffer[i++] = crc;
+    short crc = calcula_CRC(infos, 7);
+    memcpy(tx_buffer, &infos, 7);
+    memcpy(&tx_buffer[7], &crc, 2);
     printf("Buffers de memória criados!\n");
     
     if (uart0_filestream != -1)
     {
-        printf("Escrevendo caracteres na UART ...");
-        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
+        printf("Escrevendo caracteres na UART ...\n");
+        int count = write(uart0_filestream, &tx_buffer[0], 9);
         if (count < 0)
         {
             printf("UART TX error\n");
@@ -73,64 +66,66 @@ void sendMessage(char code, char subcode, int uart0_filestream ){
             printf("escrito.\n");
         }
     }
-
 }
 
-void printOutput(int option, unsigned char* buffer, int size){}
+void readOutput(int uart0_filestream){
+    if (uart0_filestream != -1)
+    {
+        printf("tentando ler\n");
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);     
+        if (rx_length < 0)
+        {
+            printf("Erro na leitura.\n");
+        }
+        else if (rx_length == 0)
+        {
+            printf("Nenhum dado disponível.\n");
+        }
+        else
+        {
+            printf("Consegui ler =)\n");
+        }
+    }else{
+        printf("Opaaaaaaa");
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
-
-    int uart0_filestream = -1;
     int option;
-    debugMenu();
-    scanf("%d", &option);
+    int uart0_filestream = -1;
+    do {
+        debugMenu();
+        scanf("%d", &option);
 
 
-    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (uart0_filestream == -1)
-    {
-        printf("Erro - Não foi possível iniciar a UART.\n");
-    }
-    else
-    {
-        printf("UART inicializada!\n");
-    }    
-    struct termios options;
-    tcgetattr(uart0_filestream, &options);
-    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
-    options.c_iflag = IGNPAR;
-    options.c_oflag = 0;
-    options.c_lflag = 0;
-    tcflush(uart0_filestream, TCIFLUSH);
-    tcsetattr(uart0_filestream, TCSANOW, &options);
+        uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+        if (uart0_filestream == -1)
+        {
+            printf("Erro - Não foi possível iniciar a UART.\n");
+        }
+        else
+        {
+            printf("UART inicializada!\n");
+        }    
+        struct termios options;
+        tcgetattr(uart0_filestream, &options);
+        options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
+        options.c_iflag = IGNPAR;
+        options.c_oflag = 0;
+        options.c_lflag = 0;
+        tcflush(uart0_filestream, TCIFLUSH);
+        tcsetattr(uart0_filestream, TCSANOW, &options);
 
-    char code = getCode(option); 
-    char subcode = getSubCode(option);
-    printf("hmm\n");
-    sendMessage(code, subcode, uart0_filestream);
+        char code = getCode(option); 
+        char subcode = getSubCode(option);
 
-    sleep(1);
-
-    // if (uart0_filestream != -1)
-    // {
-    
-    //     unsigned char rx_buffer[256];
-    //     int data;
-    //     int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);     
-    //     if (rx_length < 0)
-    //     {
-    //         printf("Erro na leitura.\n");
-    //     }
-    //     else if (rx_length == 0)
-    //     {
-    //         printf("Nenhum dado disponível.\n");
-    //     }
-    //     else
-    //     {
-    //         printOutput(option, rx_buffer, rx_length);
-    //     }
-    // }
-
+        sendMessage(code, subcode, uart0_filestream);
+        sleep(1);
+        readOutput(uart0_filestream);
+        
+    } while(option != 0);
     close(uart0_filestream);
    return 0;
 }
